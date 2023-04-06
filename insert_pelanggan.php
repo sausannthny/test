327f1f74c842ef954ecb4c2201e83ef05b29da25
sausannthny <sausantahany@gmail.com>
2023-04-06 08:26:02 +0700

    add comment

--- insert_pelanggan.php
+++ insert_pelanggan.php
@@ -3,7 +3,7 @@ require "koneksi.php";
 
     $nama_pelanggan = $_POST['nama_pelanggan'];
     $alamat = $_POST['alamat'];
-
+    //query insert
     $sql = "INSERT INTO tb_pelanggan VALUES ('', '$nama_pelanggan','$alamat')";
 
     mysqli_query($conn, $sql );
